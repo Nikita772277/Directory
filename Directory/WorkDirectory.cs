e14e5b91786d52fea485b520dba55beef8006d3c
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-11-12 10:55:29 +0300

    Добавьте файлы проекта.

---
+++ Directory/WorkDirectory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directory
+{
+    internal class WorkDirectory
+    {
+        public void CreateDirectory()
+        {
+            Console.WriteLine($"Введите название директории");
+            string enter=Console.ReadLine();
+        }
+    }
+}
