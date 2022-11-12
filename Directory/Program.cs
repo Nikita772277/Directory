7771ee0dcd42cce27bf73ceb101069c3dd35bca7
Nikita77227 <Nikita77227@DESKTOP-029TR2H>
2022-11-12 22:05:54 +0300

    123

--- Directory/Program.cs
+++ Directory/Program.cs
@@ -1,1 +1,59 @@
 ﻿
+using Directory;
+
+WorkDirectory Wdirektory=new WorkDirectory();
+void menu()
+{
+    Console.WriteLine();
+    Console.WriteLine($"1) Создать директорию");
+    Console.WriteLine($"2) Удалить директорию");
+    Console.WriteLine($"3) Проверить наличие файлов в директории");
+    Console.WriteLine($"4) Удалить какой-либо файл в директории");
+    Console.WriteLine($"5) Создать текстовый файл");
+    Console.WriteLine($"6) Прочитать информацию из какого-либо файла");
+    Console.WriteLine($"7) Записать информацию в файл");
+    Console.WriteLine();
+}
+void GetMenu()
+{
+    while (true)
+    {
+        menu();
+        string enter = Console.ReadLine();
+        bool ent = int.TryParse(enter, out int number);
+        if (ent == true)
+        {
+            if (number == 1)
+            {
+                Wdirektory.CreateDirectory();
+            }
+            else if (number == 2)
+            {
+                Wdirektory.DeleteDirectory();
+            }
+            else if (number == 3)
+            {
+                Wdirektory.GetFiles();
+            }
+            else if (number == 4)
+            {
+                Wdirektory.DeleteFile();
+            }
+            else if (number == 5)
+            {
+                Wdirektory.CreateFile();
+            }
+            else if (number == 6)
+            {
+                Wdirektory.ReadFile();
+            }
+            else if (number == 7)
+            {
+                Wdirektory.RecordInformation();
+            }
+            else { Console.WriteLine($"Выберети пункт из меню"); }
+        }
+        else { Console.WriteLine($"Выберети пункт из меню"); }
+    }
+}
+GetMenu();
--- Directory/WorkDirectory.cs
+++ Directory/WorkDirectory.cs
@@ -1,17 +1,202 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
 
 namespace Directory
 {
     internal class WorkDirectory
     {
+        private string _directory;
+        private string _name;
+        public void SetWay()
+        {
+            //Console.WriteLine($"Введите путь по котрому хотите создать директорию");
+            //string way = Console.ReadLine();
+            Console.WriteLine($"Введите название директории");
+            _name = Console.ReadLine();
+            //string directory = $@"{way}{enter}";
+            _directory = $@"C:\Users\Nikita77227\Desktop\{_name}";
+
+        }
         public void CreateDirectory()
         {
-            Console.WriteLine($"Введите название директории");
-            string enter=Console.ReadLine();
+            SetWay();
+            DirectoryInfo dirinfo = new DirectoryInfo(_directory);
+            if (!dirinfo.Exists)
+            {
+                try
+                {
+                    dirinfo.Create();
+                    Console.WriteLine($"Вы создали директорию: {_name}");
+                }
+                catch
+                {
+                    Console.WriteLine($"Не удалось создать директорию по указанному адресу");
+                }
+
+            }
+            else { Console.WriteLine($"Директория не найдена"); }
+        }
+        public void DeleteDirectory()
+        {
+            SetWay();
+            DirectoryInfo dirinfo = new DirectoryInfo(_directory);
+            if (dirinfo.Exists)
+            {
+                try
+                {
+                    dirinfo.Delete();
+                    Console.WriteLine($"Вы удалили директорию {_name}");
+                }
+                catch
+                {
+                    Console.WriteLine($"Не удалось удалить директорию по указанному адресу");
+                }
+            }
+            else { Console.WriteLine($"Директория не найдена"); }
+        }
+        public void GetFiles()
+        {
+            SetWay();
+            DirectoryInfo dirinfo = new DirectoryInfo(_directory);
+            if (dirinfo.Exists)
+            {
+                DirectoryInfo[] dirs = dirinfo.GetDirectories();
+                if (dirs == null) { Console.WriteLine($"Нет подкаталогов"); }//не работает
+                else
+                {
+                    Console.WriteLine("Подкаталоги:");
+                    foreach (DirectoryInfo dir in dirs)
+                    {
+                        Console.WriteLine(dir.Name);
+                    }
+                }
+                Console.WriteLine();
+                FileInfo[] files = dirinfo.GetFiles();
+                if (files == null) { Console.WriteLine($"Нет файлов"); }
+                else
+                {
+                    Console.WriteLine("Файлы:");
+                    foreach (FileInfo file in files)
+                    {
+                        Console.WriteLine(file.Name);
+                    }
+                }
+            }
+            else { Console.WriteLine($"Директория не найдена"); }
+        }
+        public void DeleteFile()
+        {
+            SetWay();
+            Console.WriteLine($"Введите название файла который нужно удалить");
+            string enter = Console.ReadLine();
+            string file = $@"{_directory}\{enter}.txt";
+            DirectoryInfo dirinfo = new DirectoryInfo(_directory);
+            FileInfo finfo = new FileInfo(file);
+            if (dirinfo.Exists)
+            {
+                if (finfo.Exists)
+                {
+                    finfo.Delete();
+                    Console.WriteLine($"файл {enter} удалён");
+                }
+                else { Console.WriteLine($"Файл не найден"); }
+            }
+            else { Console.WriteLine($"Директория не найдена"); }
+        }
+        public void CreateFile()
+        {
+            SetWay();
+            Console.WriteLine($"Введите название файла который хотите создать");
+            string enter = Console.ReadLine();
+            string file = $@"{_directory}\{enter}.txt";
+            DirectoryInfo dirinfo = new DirectoryInfo(_directory);
+            FileInfo finfo = new FileInfo(file);
+            if (dirinfo.Exists)
+            {
+                if (finfo.Exists)
+                {
+                    finfo.Create();
+                    Console.WriteLine($"Файл {enter} создан");
+                }
+                else { Console.WriteLine($"Файл не найден"); }
+            }
+            else { Console.WriteLine($"Директория не найдена"); }
+        }
+        public async void ReadFile()
+        {
+            SetWay();
+            Console.WriteLine($"Введите название файла который хотите считать");
+            string enter = Console.ReadLine();
+            string file = $@"{_directory}\{enter}.txt";
+            DirectoryInfo dirinfo = new DirectoryInfo(_directory);
+            FileInfo finfo = new FileInfo(file);
+            if (dirinfo.Exists)
+            {
+                if (finfo.Exists)
+                {
+                    using (StreamReader reader = new StreamReader(file))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(reader.ReadToEnd());
+                    }
+                }
+                else { Console.WriteLine($"Файл не найден"); }
+            }
+            else { Console.WriteLine($"Директория не найдена"); }
+        }
+        public async void RecordInformation()
+        {
+            SetWay();
+            Console.WriteLine($"Введите название файла в который хотите записать данные");
+            string enter = Console.ReadLine();
+            string file = $@"{_directory}\{enter}.txt";
+            DirectoryInfo dirinfo = new DirectoryInfo(_directory);
+            FileInfo finfo = new FileInfo(file);
+            if (dirinfo.Exists)
+            {
+                if (finfo.Exists)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine($"Хотите ли вы полностью перезаписать файл (введите 1)да. 2)нет)");
+                        string number = Console.ReadLine();
+                        bool numbe = int.TryParse(number, out int check);
+                        Console.WriteLine($"Введите то что хотите записать в файл");
+                        string text = Console.ReadLine();
+                        if (numbe == true)
+                        {
+                            if (check == 1)
+                            {
+                                using (StreamWriter writer = new StreamWriter(file, false))
+                                {
+                                    await writer.WriteLineAsync(text);
+                                    break;
+                                }
+                            }
+                            else if (check == 2)
+                            {
+                                using (StreamWriter writer = new StreamWriter(file, true))
+                                {
+                                    await writer.WriteLineAsync(text);
+                                    break;
+                                }
+                            }
+                            else { Console.WriteLine($"Выберите вариант из предложенных"); }
+                        }
+                        else { Console.WriteLine($"Выберите вариант из предложенных"); }
+                    }
+                }
+                else { Console.WriteLine($"Файл не найден"); }
+            }
+            else { Console.WriteLine($"Директория не найдена"); }
         }
     }
 }
